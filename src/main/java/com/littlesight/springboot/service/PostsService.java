e7a343b5b4a92a69dc2adc9e645c7d0d2430354f
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-08-17 16:29:35 +0900

    oauth2 키 추가 및 git ignore test

--- src/main/java/com/littlesight/springboot/service/PostsService.java
+++ src/main/java/com/littlesight/springboot/service/PostsService.java
@@ -35,6 +35,15 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id)
+    {
+        Posts posts = postsReporitory.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsReporitory.delete(posts);
+    }
+
     public PostsResponseDto findById(Long id)
     {
         Posts entity = postsReporitory.findById(id)
