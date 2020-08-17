09bc10e8c1be3956665614877843a1c7819f3c66
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-08-18 00:22:32 +0900

    OAuth2 네이버 로그인 연동

--- src/main/java/com/littlesight/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/littlesight/springboot/config/auth/dto/OAuthAttributes.java
@@ -29,8 +29,25 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId, String userNameAttributeName,
                                      Map<String, Object> attributes)
     {   // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 함
+        if("naver".equals(registrationId))
+            return ofNaver("id", attributes);
+
         return ofGoogle(userNameAttributeName, attributes);
     }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes)
+    {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes)
     {
         return OAuthAttributes.builder()
--- src/main/java/com/littlesight/springboot/web/IndexController.java
+++ src/main/java/com/littlesight/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.littlesight.springboot.web;
 
+import com.littlesight.springboot.config.auth.LoginUser;
 import com.littlesight.springboot.config.auth.dto.SessionUser;
 import com.littlesight.springboot.service.PostsService;
 import com.littlesight.springboot.web.dto.PostsResponseDto;
@@ -18,11 +19,9 @@ public class IndexController {
     private final PostsService postsService;
     private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model) // Model - 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
+    public String index(Model model, @LoginUser SessionUser user) // Model - 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
     {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null)
             model.addAttribute("userName", user.getName());
