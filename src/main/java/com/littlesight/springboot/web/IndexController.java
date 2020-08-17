e8199c296f86633630253a97938052b00b1110f9
gogoadl <49335446+gogoadl@users.noreply.github.com>
2020-08-17 22:49:41 +0900

    OAuth2 구글 로그인 연동

--- src/main/java/com/littlesight/springboot/web/IndexController.java
+++ src/main/java/com/littlesight/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.littlesight.springboot.web;
 
+import com.littlesight.springboot.config.auth.dto.SessionUser;
 import com.littlesight.springboot.service.PostsService;
 import com.littlesight.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +9,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
-
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) // Model - 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다.
     {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null)
+            model.addAttribute("userName", user.getName());
+
         return "index";
     }
 
@@ -34,4 +43,5 @@ public class IndexController {
 
         return "posts-update";
     }
+
 }
