86fdc9c178b7f96f6e982272159eba07bafbc860
HyeonWoo Park <49335446+gogoadl@users.noreply.github.com>
2024-04-14 15:35:40 +0900

    Revert "initial commit"
    This reverts commit b116772107f0cd1a95f47f2e72856e4a55d2512c.

---
+++ src/main/java/com/littlesight/springboot/web/PostsApiController.java
@@ -0,0 +1,44 @@
+package com.littlesight.springboot.web;
+
+import com.littlesight.springboot.web.dto.PostsResponseDto;
+import com.littlesight.springboot.web.dto.PostsSaveRequestDto;
+import com.littlesight.springboot.service.PostsService;
+import com.littlesight.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto)
+    {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto)
+    {
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id)
+    {
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id)
+    {
+        return postsService.findById(id);
+    }
+
+
+
+}
